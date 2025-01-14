4055b469789aff2f8c377b7834e89994228d7f9b
sohamPatkar <spatkar398@gmail.com>
2025-01-14 14:01:58 +0530

    Added BodyPart Drawing

--- Linked-List-Snake/include/Player/BodyPart.h
+++ Linked-List-Snake/include/Player/BodyPart.h
@@ -9,15 +9,23 @@ namespace Player
 	class BodyPart
 	{
 	private:
-		UI::UIElement::ImageView* body_part_image;
+		UI::UIElement::ImageView* bodypart_image;
 		sf::Vector2i grid_position;
 		Direction direction;
 
 		float bodypart_width;
 		float bodypart_height;
 
+		void createBodyPartImage();
+		void initializeBodyPartImage();
+		sf::Vector2f getBodyPartScreenPosition();
+		void destroy();
+
 	public:
-		void initialize();
+		BodyPart();
+		~BodyPart();
+
+		void initialize(float width, float height, sf::Vector2i pos, Direction dir);
 		void update();
 		void render();
 
--- Linked-List-Snake/source/Player/BodyPart.cpp
+++ Linked-List-Snake/source/Player/BodyPart.cpp
@@ -1,1 +1,58 @@
 #include "../../include/Player/BodyPart.h"
+#include "../../include/Global/Config.h"
+
+namespace Player
+{
+	BodyPart::BodyPart()
+	{
+		grid_position = sf::Vector2i(0, 0);
+
+		createBodyPartImage();
+	}
+
+	BodyPart::~BodyPart()
+	{
+		destroy();
+	}
+
+	void BodyPart::initialize(float width, float height, sf::Vector2i pos, Direction dir)
+	{
+		bodypart_height = height;
+		bodypart_width = width;
+		grid_position = pos;
+		direction = dir;
+
+		initializeBodyPartImage();
+	}
+
+	void BodyPart::update()
+	{
+
+	}
+
+	void BodyPart::render()
+	{
+		bodypart_image->render();
+	}
+
+	void BodyPart::createBodyPartImage()
+	{
+		bodypart_image = new UI::UIElement::ImageView();
+	}
+
+	void BodyPart::initializeBodyPartImage()
+	{
+		bodypart_image->initialize(Global::Config::snake_body_texture_path, bodypart_width, bodypart_height, static_cast<sf::Vector2f>(grid_position));
+		bodypart_image->setOriginAtCentre();
+	}
+
+	sf::Vector2f BodyPart::getBodyPartScreenPosition()
+	{
+		return sf::Vector2f(0, 0);
+	}
+
+	void BodyPart::destroy()
+	{
+		delete(bodypart_image);
+	}
+}
